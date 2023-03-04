95511ff4b5c2fdded0f48a77d4ee2bd53dbe270a
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-03-04 07:50:48 -0300

    :sparkles: feat: Adicionando Botoes.js

---
+++ quiz/src/components/Botoes.js
@@ -0,0 +1,23 @@
+function Botoes() {
+    return (
+        <>
+            <div>
+                <button>Botao 1</button>  
+            </div>
+            <div>
+                <button>Botao 2</button>
+            </div>
+            <div>
+                <button>Botao 3</button>
+            </div>
+            <div>
+                <button>Botao 4</button>
+            </div>
+            <div>
+                <button>Botao 5</button>
+            </div>
+        </>
+    )
+}
+
+export default Botoes
