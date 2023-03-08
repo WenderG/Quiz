a51df5c572721f17cb95fd0cfaf49997a3cda59d
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-03-08 10:53:42 -0300

    :books: docs: Edições

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -5,36 +5,45 @@ import '../Css/Style.css'
 export default function Jogo() {
     const questions = Quiz ?? []
     const [questaoAtual, setQuestaoAtual] = useState(0);
+    const [showAcertou, setShowAcertou] = useState(false);
+    const [pontos, setPontos] = useState(0);
 
-    function proximaQuestao(correta) {
-        const nextQuestion = questaoAtual + 1
-
+    function proximaQuestao(afirmativa) {
+        const nextQuestion =  questaoAtual + 1;
+    
+        if (afirmativa){
+            setPontos(pontos + 1);
+        }
+    
         if (nextQuestion < questions.length) {
             setQuestaoAtual(nextQuestion);
+        } else {
+            setShowAcertou(true);
         }
-    }
-
-    console.log(questions[questaoAtual])
+      }
 
     return (
         <div className='container'>
-        {(
-            <>
-            <div className='infoQuestoes'>
-                <div className="contagemQuestoes">
-                <span>Questão {questaoAtual + 1}/{questions.length}</span>
-                </div >
-                <div className="questao">{questions[questaoAtual].questao}</div>
-            </div >
-            <div className="resposta">
-                {questions[questaoAtual].respostas.map((opcoesResposta) =>
-                <div className="grupoResposta">
-                    <span>{opcoesResposta.alternativa}</span>
-                    <button onClick={() => proximaQuestao(opcoesResposta.correta)}>{opcoesResposta.resposta}</button>
-                </div>)}
-            </div>
-            </>
-        )}
+        {showAcertou ? (
+        <div className='pontuacao'>
+          <span>Sua pontuação é {pontos} de {questions.length}</span>
+        </div>) : (
+                <>
+                    <div className='infoQuestoes'>
+                        <div className="contagemQuestoes">
+                        <span>Questão {questaoAtual + 1}/{questions.length}</span>
+                        </div >
+                        <div className="questao">{questions[questaoAtual].questao}</div>
+                    </div >
+                    <div className="resposta">
+                        {questions[questaoAtual].respostas.map((opcoesResposta) =>
+                        <div className="grupoResposta">
+                            <span>{opcoesResposta.alternativa}</span>
+                            <button onClick={() => proximaQuestao(opcoesResposta.afirmativa)}>{opcoesResposta.resposta}</button>
+                        </div>)}
+                    </div>
+                </>
+            )}
         </div >
     )
 }
